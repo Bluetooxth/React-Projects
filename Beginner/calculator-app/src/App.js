2315ac30b315fd1cb6ff96ff09f1ac81980f8544
Priyanshu Chahar <165533860+Priyannxhuu@users.noreply.github.com>
2024-06-02 12:06:47 +0000

    react

--- Beginner/calculator-app/src/App.js
+++ Beginner/calculator-app/src/App.js
@@ -1,12 +1,8 @@
-import React , {useState , useCallback} from "react";
+import React, { useState } from "react";
 
 const App = () => {
 
   const buttons = [
-    { id: 'clear', sign: 'C', type: 'function' },
-    { id: 'delete', sign: 'DEL', type: 'function' },
-    { id: 'divide', sign: '/', type: 'operator' },
-    { id: 'none', sign: '', type: '' },
     { id: 'seven', sign: '7', type: 'number' },
     { id: 'eight', sign: '8', type: 'number' },
     { id: 'nine', sign: '9', type: 'number' },
@@ -19,19 +15,41 @@ const App = () => {
     { id: 'two', sign: '2', type: 'number' },
     { id: 'three', sign: '3', type: 'number' },
     { id: 'add', sign: '+', type: 'operator' },
-    { id: 'zero', sign: '0', type: 'number' },
     { id: 'decimal', sign: '.', type: 'number' },
+    { id: 'zero', sign: '0', type: 'number' },
+    { id: 'clear', sign: 'C', type: 'function' },
+    { id: 'delete', sign: 'DEL', type: 'function' },
+    { id: 'divide', sign: '/', type: 'operator' },
     { id: 'equals', sign: '=', type: 'function' }
-  ];  
+  ];
 
-  return(
+  const [input, setInput] = useState("")
+
+  const handleclick = (sign) => {
+    switch (sign) {
+      case "DEL":
+        setInput(input.slice(0, -1))
+        break;
+      case "C":
+        setInput("")
+        break;
+      case "=":
+        setInput(eval(input).toString())
+        break;
+      default:
+        setInput((prevInput) => prevInput + sign)
+        break;
+    }
+  }
+
+  return (
     <main className='flex flex-col justify-center items-center w-full h-screen'>
       <div className='flex flex-col justify-center items-center p-7 gap-5 rounded-md bg-slate-100'>
         <h1 className="text-4xl font-bold">Claculator</h1>
-        <input className="p-3 bg-gray-800 rounded-lg outline-none text-3xl font-semibold text-slate-200"></input>
+        <input className="p-3 bg-gray-800 rounded-md outline-none text-3xl font-semibold text-slate-200" value={input}></input>
         <div className="grid grid-cols-4 justify-center items-center gap-1 w-full">
-          {buttons.map((btn , index) => (
-            <span key={index} className="p-3 bg-gray-800 text-slate-300 flex justify-center items-center text-center font-medium cursor-pointer rounded-lg">
+          {buttons.map((btn, index) => (
+            <span key={index} onClick={() => handleclick(btn.sign)} className="p-3 bg-gray-800 hover:bg-gray-900 text-slate-300 flex justify-center items-center text-center font-medium cursor-pointer rounded-md">
               {btn.sign}
             </span>
           ))}
