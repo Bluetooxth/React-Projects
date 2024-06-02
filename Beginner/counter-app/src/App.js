56fcc96eb1ab93915e9344d11e7d782c52660258
Priyanshu Chahar <165533860+Priyannxhuu@users.noreply.github.com>
2024-06-02 05:27:20 +0000

    react

--- Beginner/counter-app/src/App.js
+++ Beginner/counter-app/src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './ui/Button';
 
 const App = () => {
+
+    const[count , setCount] = useState(0);
+
+    const increase = () => {
+        setCount(count+1)
+    }
+
+    const decrease = () => {
+        setCount(count-1)
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
+
     return (
-        <main>
-            <Button className="px-3 py-2 rounded-lg font-medium text-xl bg-slate-200 hover:bg-slate-300 transition ease-in-out">
-                Hello This is a Button
-            </Button>
-            <button>Button</button>
+        <main className='flex flex-col justify-center items-center w-full h-screen'>
+            <div className='flex flex-col justify-center items-center p-7 rounded-md bg-slate-100'>
+            <h1 className='text-5xl font-semibold text-center flex flex-col gap-5'>Count
+            <h1 className='text-6xl'>{count}</h1>
+            </h1>
+            <div className='flex gap-5 mt-7'>
+                <Button onClick={decrease} className="px-3 py-2 rounded-lg font-medium text-slate-200 bg-slate-700 hover:bg-slate-800 transition ease-in-out">
+                    Decrease
+                </Button>
+                <Button onClick={reset} className="px-3 py-2 rounded-lg font-medium text-slate-200 bg-slate-700 hover:bg-slate-800 transition ease-in-out">
+                    Reset
+                </Button>
+                <Button onClick={increase} className="px-3 py-2 rounded-lg font-medium text-slate-200 bg-slate-700 hover:bg-slate-800 transition ease-in-out">
+                    Increase
+                </Button>
+            </div>
+            </div>
         </main>
     );
 };
